0be5b30a24ae2ecabf79761968df1b686776cd11
willyishmael <wtangka22@gmail.com>
2023-08-25 16:48:03 +0800

    setup router

---
+++ tokopedia-play-clone-fe-alt/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className='w-full p-4'>
+            <h2 className='text-xl font-bold text-start text-green-400'>Tokopedia Play Clone</h2>
+            <h6 className="text-s font-light font font-gray-400">by William Tangka</h6>
+        </header>
+    )
+}
---
+++ tokopedia-play-clone-fe-alt/src/pages/DetailVideo.tsx
@@ -0,0 +1,5 @@
+export default function DetailVideo() {
+    return(
+        <></>
+    )
+}
