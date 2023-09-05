1a3ef593df0aff33a08b5774ddf948d1ef29c80c
willyishmael <wtangka22@gmail.com>
2023-09-05 20:57:45 +0800

    Update ErrorHandler.ts

--- tokopedia-play-clone-be/src/middlewares/ErrorHandler.ts
+++ tokopedia-play-clone-be/src/middlewares/ErrorHandler.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import Logging from "../lib/Logging";
+import Logging from "../lib/Logging.js";
 
 export function errorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
     Logging.error(`Path: ${req.path}`);
