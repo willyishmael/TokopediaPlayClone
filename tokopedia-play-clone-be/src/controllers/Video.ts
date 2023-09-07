ee64dbc0f3a2b331e5d15ccae4183aa5823b1550
willyishmael <wtangka22@gmail.com>
2023-09-07 14:54:52 +0800

    fix video controller

--- tokopedia-play-clone-be/src/controllers/Video.ts
+++ tokopedia-play-clone-be/src/controllers/Video.ts
@@ -1,10 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import Video from "../models/Video.js";
-import Logging from "../lib/Logging.js";
 
 const createVideo = async (req: Request, res: Response, next: NextFunction) => {
-    Logging.info('Create video executed')
     const { title, video_url, thumbnail_url } = req.body
 
     const video = new Video({
@@ -14,48 +12,44 @@ const createVideo = async (req: Request, res: Response, next: NextFunction) => {
 
     return await video
         .save()
-        .then((video) => res.status(201).json({ video }))
-        .catch((error) => res.status(500).json({ error }))
+        .then((video) => res.status(201).json({ data: video }))
+        .catch((error) => next(error))
 }
 
 const readVideo = async (req: Request, res: Response, next: NextFunction) => {
-    Logging.info('Read video by id executed')
     const { videoId } = req.params
 
     return await Video.findById(videoId)
-        .then((video) => {(video 
-            ? res.status(201).json({ video }) 
-            : res.status(404).json({ message: "Not found" }))})
-        .catch((error) => res.status(500).json({ error }))
+        .then((video) => video 
+            ? res.status(201).json({ data: video }) 
+            : res.status(404).json({ error: "Not found" }))
+        .catch((error) => next(error))
 }
 
 const readAllVideo = async (req: Request, res: Response, next: NextFunction) => {
-    Logging.info('Read all video executed')
     return await Video.find()
-        .then((videos) => res.status(201).json({ videos }))
-        .catch((error) => res.status(500).json({ error }))
+        .then((videos) => res.status(201).json({ data: videos }))
+        .catch((error) => next(error))
 }
 
 const updateVideo = async (req: Request, res: Response, next: NextFunction) => {
-    Logging.info('Update video executed')
-    const videoId = req.params.videoId
+    const { videoId } = req.params
 
     return await Video.findByIdAndUpdate(videoId, req.body)
-        .then((video) => (video 
-            ? res.status(201).json({ message: 'Updated', data: video }) 
-            : res.status(404).json({ message: "Not found" })))
-        .catch((error) => res.status(500).json({ error }))
+        .then((video) =>video
+            ? res.status(201).json({ message: 'Updated', data: video })
+            : res.status(404).json({ error: "Not found" }))
+        .catch((error) => next(error))
 }
 
 const deleteVideo = async (req: Request, res: Response, next: NextFunction) => {
-    Logging.info('Delete video executed')
-    const videoId = req.params.videoId
+    const { videoId } = req.params
 
     return await Video.findByIdAndDelete(videoId)
-        .then((video) => (video 
-            ? res.status(201).json({ message: 'Deleted' }) 
-            : res.status(404).json({ message: "Not found" })))
-        .catch((error) => res.status(500).json({ error }))
+        .then((video) => video
+            ? res.status(201).json({ message: 'Deleted' })
+            : res.status(404).json({ error: "Not found" }))
+        .catch((error) => next(error))
 }
 
 export default { createVideo, readVideo, readAllVideo, updateVideo, deleteVideo }
